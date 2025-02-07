ded173710c20b7a3aeff995cf0d62e7aaa0c4ed5
kunmingL <770320622@qq.com>
2025-02-07 18:12:34 +0800

    @task_id@km@demo

--- elearn-api/src/main/java/com.changjiang.elearn.api/service/SpokenEnglish.java
+++ elearn-api/src/main/java/com.changjiang.elearn.api/service/SpokenEnglish.java
@@ -1,8 +1,19 @@
 package com.changjiang.elearn.api.service;
 
+import com.changjiang.bff.annotation.ServiceConfig;
+import com.changjiang.bff.constants.SrvChannel;
+
 /**
  * 英语智能对话
  */
 public interface SpokenEnglish {
+    /**
+     * 英语智能对话
+     * @param text
+     * @return
+     */
+    @ServiceConfig(registryId = "demo.api.test",
+            url = "/api/demo/test",
+            channel = {SrvChannel.PC, SrvChannel.MOBILE})
     String spokenEnglish(String text);
 }
