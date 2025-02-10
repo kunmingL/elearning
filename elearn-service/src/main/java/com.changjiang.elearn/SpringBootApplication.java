d7df5226c4bb94185d5e14887ee9cd0e7e2e9773
kunmingL <770320622@qq.com>
2025-02-10 19:33:10 +0800

    @task_id@km@demo

--- elearn-service/src/main/java/com.changjiang.elearn/SpringBootApplication.java
+++ elearn-service/src/main/java/com.changjiang.elearn/SpringBootApplication.java
@@ -2,8 +2,12 @@ package com.changjiang.elearn;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.changjiang.grpc"
+})
 public class SpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApplication.class, args);
---
+++ elearn-service/src/main/java/com.changjiang.elearn/application/service/impl/SpokenEnglishImpl.java
@@ -0,0 +1,20 @@
+package com.changjiang.elearn.application.service.impl;
+
+import com.changjiang.elearn.api.service.SpokenEnglish;
+import com.changjiang.grpc.annotation.GrpcService;
+
+import java.rmi.registry.Registry;
+
+@GrpcService
+public class SpokenEnglishImpl implements SpokenEnglish {
+
+    @Override
+    public String spokenEnglish(String text) {
+        return "hello world";
+    }
+
+    @Override
+    public String spokenEnglishNone(String text) {
+        return "";
+    }
+}
