6c5e210048ec10dfa994c72addcf350758cb771a
kunmingL <770320622@qq.com>
2025-02-10 23:08:04 +0800

    @task_id@km@后端服务

--- elearn-service/src/main/java/com.changjiang.elearn/SpringBootApplication.java
+++ elearn-service/src/main/java/com.changjiang.elearn/SpringBootApplication.java
@@ -1,13 +1,16 @@
 package com.changjiang.elearn;
 
 
+import com.changjiang.grpc.annotation.EnableGrpcService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
 @ComponentScan(basePackages = {
+        "com.changjiang.elearn",
         "com.changjiang.grpc"
 })
+@EnableGrpcService
 public class SpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApplication.class, args);
